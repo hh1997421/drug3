41eadc1f5cd7d482d11a065738ca3b8a6807cc36
hh1997421 <haruto.10120815@gmail.com>
2020-11-19 18:03:46 +0900

    修正完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'rooms/show'
-  get 'tweets/new'
   get 'messages/index'
   get 'contacts/new'
   devise_for :users
@@ -9,6 +8,5 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :edit]
   resources :users, only: [:edit, :update]
   resources :messages, only: [:new, :create, :edit, :update]
-
   resources :tweets, only: [:new, :create]
 end
