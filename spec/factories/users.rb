66fcb38f5f85cc292c5114cda4c6c4bb014684f8
hh1997421 <haruto.10120815@gmail.com>
2020-11-26 11:23:31 +0900

    環境変数

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Nickname.nickname}
-    email {Faker::Internet.free_email}
+    nickname {"yamada"}
+    email {"abab@gmail.com"}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "ユーザーログイン機能", type: :system do
+  it 'ログインしていない状態でトップページにアクセスした場合、サインインページに移動する' do
+    # トップページに遷移する
+    visit root_path
+  end
+
+  it 'ログインに成功し、トップページに遷移する' do
+    # 予め、ユーザーをDBに保存する
+    @user = FactoryBot.create(:user)
+    # サインインページへ移動する
+    visit  new_user_session_path
+  end
+
+  it 'ログインに失敗し、再びサインインページに戻ってくる' do
+    # 予め、ユーザーをDBに保存する
+    @user = FactoryBot.create(:user)
+    # トップページに遷移する
+    visit root_path
+  end
+end
