98f1c93ca1a69fa39efbb4cd9e766c226716f0ca
hh1997421 <haruto.10120815@gmail.com>
2020-12-08 14:41:33 +0900

    追加7

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ Product.create(status:'頭痛', name:'タイレノール', price:'1000', sleep:'
 Product.create(status:'頭痛', name:'イブクイック頭痛薬DX', price:'2000', sleep:'あり', information:'https://lohaco.jp/product/2648383')
 Product.create(status:'ストレス', name:'半夏厚朴湯', price:'3000', sleep:'なし', information:'https://www.kracie.co.jp/ph/k-therapy/product/symptom/stress/?_ga=2.125617226.1360220208.1604898102-100837335.1603968146')
 Product.create(status:'肌トラブル', name:'黄連解毒湯', price:'1000', sleep:'なし', information:'https://www.kracie.co.jp/ph/k-therapy/product/symptom/skin/?_ga=2.129959116.1360220208.1604898102-100837335.1603968146')
+Product.create(status:'肌トラブル', name:'ペアA錠 ', price:'1000', sleep:'なし', information:'https://www.yodobashi.com/product/100000001002273252/')
 Product.create(status:'鼻炎', name:'小青竜湯', price:'1000', sleep:'なし', information:'https://www.kracie.co.jp/ph/k-therapy/product/symptom/skin/?_ga=2.129959116.1360220208.1604898102-100837335.1603968146')
 Product.create(status:'鼻炎', name:'アレジオン', price:'2000', sleep:'なし', information:'https://minacolor.com/items/6054/')
 Product.create(status:'風邪', name:'葛根湯', price:'1000', sleep:'なし', information:'https://www.tsumura.co.jp/kampo/list/detail/001.html')
